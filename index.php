7dab64c447686d6b21c0c281b6f4bedaeb0ad3f0
akis94 <akispro94@gmail.com>
2014-08-10 11:37:01 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Hello World</title>
+    <script>
+      console.log("Hello World!");
+    </script>
+  </head>
+  <body>
+    echo "<h2 style="color:red;">Hello World!</h2>";
+  </body>
+</html>
